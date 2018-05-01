d8f0bbbdc08a30a464d4df8c655eeb744c4276de
Anjana S <anjanas@thoughtworks.com>
2018-05-02 01:48:44 +0530

    BFS

--- src/main/scala/MostPopularSuperHero.scala
+++ src/main/scala/MostPopularSuperHero.scala
@@ -2,7 +2,7 @@ import org.apache.log4j._
 import org.apache.spark._
 
 object MostPopularSuperHero {
-  def parseSuperHero(line:String ): Option[(Int, String)] = {
+  def parseSuperHero(line: String): Option[(Int, String)] = {
     val index = line.indexOf(' ')
     if (index > 0) {
       return Some(line.substring(0, index).toInt -> line.substring(index + 1))
@@ -18,12 +18,12 @@ object MostPopularSuperHero {
     val superHeroes = sc.textFile("src/resources/Marvel-names.txt")
       .flatMap(parseSuperHero)
     val famouseOne = sc.textFile("src/resources/Marvel-graph.txt")
-      .map(x=>{
-      val data = x.toString().split(" ");
-      (data(0).toInt, data.size)
-    }).reduceByKey( (x,y) => x + y )
+      .map(x => {
+        val data = x.toString().split(" ");
+        (data(0).toInt, data.size)
+      }).reduceByKey((x, y) => x + y)
       .map(x => (x._2, x._1))
-        .max()
+      .max()
     println(s"${superHeroes.lookup(famouseOne._2)(0)} is the most famous fello :P")
   }
 }
