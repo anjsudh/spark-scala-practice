420191a6426b4aa8cac7598340c39b3ef14d7de4
Anjana S <anjanas@thoughtworks.com>
2018-05-01 22:09:13 +0530

    sorting, regex for extractions

--- src/main/scala/WordCounting.scala
+++ src/main/scala/WordCounting.scala
@@ -1,17 +1,20 @@
-import java.lang.System.currentTimeMillis
-
 import org.apache.log4j._
 import org.apache.spark._
 
 object Wordcounting {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
-    val sc = new SparkContext("local[*]", "Word Counting")
+    val sc = new SparkContext("local", "Word Counting")
     val lines = sc.textFile("src/resources/book.txt")
     val words = lines
-      .flatMap(x => {x.split(" ")})
-      .map(x=> x.toUpperCase())
-    words.foreach(x => println(x))
-    words.countByValue().foreach(println)
+      .flatMap(x => {
+        x.split("\\W+")
+      })
+      .map(x => x.toLowerCase())
+      .map(x => (x, 1.toFloat)).reduceByKey((x, y) => x + y)
+      .map(x=>(x._2, x._1))
+      .sortByKey(false)
+      .map(x=>(x._1, x._2))
+      .foreach(println)
   }
 }
